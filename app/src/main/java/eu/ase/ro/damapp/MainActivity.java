b5fb0de255f9cfc5c54086473e374bea5cbc617b
Andrei Serban <andyspeed2003@gmail.com>
2024-10-25 08:53:23 +0300

    fragments

--- app/src/main/java/eu/ase/ro/damapp/MainActivity.java
+++ app/src/main/java/eu/ase/ro/damapp/MainActivity.java
@@ -13,6 +13,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
+import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -21,11 +22,16 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import eu.ase.ro.damapp.databinding.ActivityMainBinding;
+import eu.ase.ro.damapp.fragments.AboutFragment;
+import eu.ase.ro.damapp.fragments.HomeFragment;
+import eu.ase.ro.damapp.fragments.ProfileFragment;
 
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
+
+    private Fragment currentFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,14 +49,27 @@ public class MainActivity extends AppCompatActivity {
         return item -> {
             if (item.getItemId() == R.id.nav_home) {
                 Toast.makeText(MainActivity.this, R.string.home_clicked, Toast.LENGTH_SHORT).show();
+                currentFragment = new HomeFragment();
             } else if (item.getItemId() == R.id.nav_profile) {
                 Toast.makeText(MainActivity.this, R.string.profile_clicked, Toast.LENGTH_SHORT).show();
+                currentFragment = new ProfileFragment();
+
             } else {
                 Toast.makeText(MainActivity.this, R.string.about_clicked, Toast.LENGTH_SHORT).show();
+                currentFragment = new AboutFragment();
+
             }
             drawerLayout.closeDrawer(GravityCompat.START);
+            openFragment();
             return true;
         };
+    }
+
+    private void openFragment() {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.main_fl_container, currentFragment)
+                .commit();
     }
 
     private void configNavigation() {
